81c6ed4d3ce1822c6df188308940d29a74d9fb27
Justin Charles <143245796+justin212407@users.noreply.github.com>
2024-08-21 21:52:25 +0000

    variables completed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let account = "justin@google.com"
+var accountPassword ="12345"
+let accountState;
+accountCity = "Jaipur"
+//accountId = 2//not allowed
+accountEmail = "hscfg32"
+accountPassword = "21234455"
+accountCity = "Bangalore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
