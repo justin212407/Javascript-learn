acd0994ac8cc3113cc3f84779288a49547871331
Justin Charles <143245796+justin212407@users.noreply.github.com>
2024-08-22 05:24:59 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all JS code as newer js
+//alert() // we are using nodejs not browser
+console.log(3+
+    3)// code readabilty should be high
+console.log("Justin");
+
+let name = "justin"
+let age = 19
+let isLoggedIn = false
+let state;
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean=> true/false
+//null => standalone value
+//undefined =>
+//symbol => uniqueness
+//object
+console.log(typeof undefined);
+console.log(typeof null);
+
