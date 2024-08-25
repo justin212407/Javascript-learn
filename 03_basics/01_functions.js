f374a2d6f61af5aa85ec211cc4f51173e75bb8e0
Justin Charles <143245796+justin212407@users.noreply.github.com>
2024-08-25 12:08:28 +0000

    functions2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,4 +34,30 @@ if(!username){
     return `${username} just logged in`
 }
 //console.log(loginUserMessage("justin"))
-console.log(loginUserMessage())
+//console.log(loginUserMessage())
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user ={
+    username : "justin",
+    price : 199 
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+handleObject({
+    username : "sam",
+    price : 399
+})
+
+const myNewArray = [200, 400, 100, 1600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue([200, 400, 100, 1000]));
