d46c22bacd0b07e34d624387e38befa0131fa75c
Justin Charles <143245796+justin212407@users.noreply.github.com>
2024-08-25 10:22:55 +0000

    json and object

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -33,7 +33,7 @@ const obj4 = {
 //const obj3 = { obj1, obj2 }
 //const obj3 = Object.assign({}, obj1, obj2, obj4)
 const obj3 ={...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
 
 
 const users = [ 
@@ -54,9 +54,34 @@ const users = [
 users[1].email
 //console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
 
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor :"hitesh"
+}
+//course.courseInstructor
+const {courseInstructor: instructor} = course
+//console.log(courseInstructor);
+console.log(instructor);
+
+//api:
+// {
+//     name : "justin"
+//     coursename : "js in hindi"
+//     price : "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
