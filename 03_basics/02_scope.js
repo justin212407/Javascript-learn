bd51b4253e431a7fd87199adabe3fa67b9bd7ec1
Justin Charles <143245796+justin212407@users.noreply.github.com>
2024-08-25 22:14:17 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -3,16 +3,49 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER:", a);
+    //console.log("INNER:", a);
+}
     
 for (let i = 0; i<Array.length; i++){
-    const element = array[i];    
+    const element = Array[i];    
+}
+
+//console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one (){
+    const username = "justin"
+    function two(){
+        const website = "youtube"
+        //console.log(username);
+    }
+    //console.log(website);
+    //two()
+}
+//one()
+
+if (true){
+    const username =  "justin"
+    if(username === "justin"){
+        const website = " youtube"
+        //console.log(username + website); 
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+//++++++++++++++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5));
+function addOne(num){
+    return num + 1
 }
 
 
-
-
-console.log(a);
-// console.log(b);
-// console.log(c);
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
 
