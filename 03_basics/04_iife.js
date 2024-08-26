fa64f4ab58b8ba14f9d1f65b4f3da560ef05adf5
Justin Charles <143245796+justin212407@users.noreply.github.com>
2024-08-26 14:09:56 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,11 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+(
 function chai(){
+    //named IIFE
     console.log(`DB CONNECTED`);  
-}
-chai()
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+
+})("justin")
