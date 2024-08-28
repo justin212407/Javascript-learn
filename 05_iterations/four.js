a02e66f18732c986f63fcc38c1386bb868467417
Justin Charles <143245796+justin212407@users.noreply.github.com>
2024-08-28 04:22:12 +0000

    for loop types

---
+++ 05_iterations/four.js
@@ -0,0 +1,32 @@
+const myObject = {
+    js : 'javascript',
+    cpp : 'C++',
+    rb : 'Ruby',
+    py : 'Python'
+}
+for (const key in myObject) {
+    //console.log(myObject[key]);
+    //console.log(`${key} shortcut is for${myObject[key]}`);
+}
+
+const programmming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programmming) {
+//    console.log(programmming[key]);
+}
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('FR', "France")
+map.set("USA", "United States of America")
+map.set('IN', "India")
+for (const key in map) {
+    // console.log(key);
+    }
+
+
+
+
+
+
+
