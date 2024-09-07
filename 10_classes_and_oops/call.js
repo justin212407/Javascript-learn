12d1e99506fea8984342ef01335b094ddbd04f01
Justin Charles <143245796+justin212407@users.noreply.github.com>
2024-09-07 08:48:51 +0000

    call and this

---
+++ 10_classes_and_oops/call.js
@@ -0,0 +1,15 @@
+function SetUsername(username){
+    //complex DB calls
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password){
+    SetUsername.call(this, username)
+
+    this.email = email
+    this.password = password
+}
+
+const chai = new creatUser("coffee", "coffee@fb.com", "12345")
+console.log(coffee);
