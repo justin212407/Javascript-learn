5988aeab529f619b5923236467ef5a3029ef9c0a
Justin Charles <143245796+justin212407@users.noreply.github.com>
2024-09-07 08:37:07 +0000

    oop

--- 10_classes_and_oops/oop.js
+++ 10_classes_and_oops/oop.js
@@ -12,3 +12,20 @@ const user = {
 console.log(user.username);
 //console.log(user.getUserDetails());
 console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    //return this
+    this.greeting = function(){
+        console.log(("Welcome ${this.username"));
+    }
+}
+
+const userOne = User("jusitn", 15, true)
+const userTwo = User("Jason", 13, false)
+console.log(userOne);
+console.log(userTwo);
